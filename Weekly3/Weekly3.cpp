ef22678514926dd1f74d54f02f50dcd9ee95949c
Mathias <Cliffback@users.noreply.github.com>
2020-09-15 22:38:44 +0200

    finished the weekly with difficulty and high scores

--- Weekly3/Weekly3.cpp
+++ Weekly3/Weekly3.cpp
@@ -1,46 +1,76 @@
-
 #include <iostream>
 #include <conio.h>
 #include <cstdlib>
 
 
-
 void task1()
 {
-	int tries = 0; // This is for counting how many tries has been done
-	int count = 0; // This is for counting how many guesses has been done inside one loop
+	int tries = 1; // This is for counting how many tries has been done
+	int count = 1; // This is for counting how many guesses has been done inside one loop
 
-	int highscoreTable[5][2]
+	int highscoreTable[100][2] // Making a table for storing highscores
 	{
-		{1, 0},
-		{2, 0},
-		{3, 0},
-		{4, 0}
 	};
-	
-	int getRandomNumber = rand() %10 + 1; // For generating a random number
-	
-	int playerGuess = 11; // For storing the player guess
 
-	std::cout << "Guess a number between 1 and 10 (0 to exit): ";
+	for (int j = 1; j <= 99; j++)
+	{
+		highscoreTable[j][0] = j;
+		highscoreTable[j][1] = 0;
+	}
+
+	int difficulty;
+	int getRandomNumber = 0;
+	std::cout << "\nDifficulties:";
+	std::cout << "\n1 = Easy - A number between 1 and 5";
+	std::cout << "\n2 = Medium - A number between 1 and 10";
+	std::cout << "\n3 = Hard - A number between 1 and 30";
+	std::cout << "\nChoose a difficulty by typing the corresponding number: ";
+	std::cin >> difficulty;
+	
+	int playerGuess = 0; // For storing the player guess
+	system("CLS");
+
+	std::cout << "Guess a number (Press 0 to exit): ";
 
 	do 
 	{
+		if (difficulty == 1)
+		{
+			getRandomNumber = rand() % 5 + 1; // For generating a random number
+
+		}
+		else if (difficulty == 2)
+		{
+			getRandomNumber = rand() % 10 + 1; // For generating a random number
+
+		}
+		else if (difficulty == 3)
+		{
+			getRandomNumber = rand() % 30 + 1; // For generating a random number
+
+		}
+		else
+		{
+			std::cout << "\nError! Please try again";
+			task1();
+		}
 		std::cin >> playerGuess;
 
 
 		if (playerGuess == getRandomNumber) // Checking if guess is right
 		{
 			system("CLS");
-			std::cout << "\nCongratulations, you guessed the right number!\n";
+			
+			count++;
+			std::cout << "Congratulations, you guessed the right number!\n";
 			std::cout << "It took you " << count << " tries!\n";
 			highscoreTable[tries][1] = count;
 			
 			std::cout << "\nHigh scores:\n";
 
-			for (int i = 0; i <= tries; i++)
+			for (int i = 0; i < tries; i++)
 			{
-				std::cout << "Game " << highscoreTable[i][0] << " : " << highscoreTable[i][1] << "\n";
+				std::cout << "Game " << highscoreTable[i+1][0] << " : " << highscoreTable[i+1][1] << "\n";
 			}
 			std::cout << "\n";
 			
@@ -49,11 +79,11 @@ void task1()
 			tries++; // add one to number of tries
 			playerGuess = 11; // Reset player guess
 			count = 0; // reset count from this round
-			getRandomNumber = rand() % 10 + 1; // set a new random number
-
 			char something = _getch();
 			std::cout << "\n";
-			std::cout << "Guess a number between 1 and 10 (0 to exit): ";
+
+			system("CLS");
+			std::cout << "Guess a number (Press 0 to exit): ";
 
 		}
 
@@ -71,36 +101,7 @@ void task1()
 }
 int main()
 {
-	std::srand(5);
-	int input = 0;
-
-
-	do
-	{
-		system("CLS");
-		input = 0;
-		std::cout << "Weekly 3: Guess a number - Mathias Eek\n";
-		std::cin >> input;
-		switch (input)
-		{
-		case 1:
-			task1();
-			break;
-		default:
-			break;
-		}
-
-	} while (input > 0 && input < 4);
-		return 0;
+	std::cout << "Weekly 3: Guess a number - Mathias Eek\n";
+	task1();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
